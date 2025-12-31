9b27dd2a5044f95f5427cf4814bb2550a720c6ae
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-31 15:05:26 +0900

    add demo text output process

--- chapter5/apply_chapter5.py
+++ chapter5/apply_chapter5.py
@@ -1,9 +1,12 @@
+import tiktoken
 import torch
-from chapter4parts import GPTModel
+from chapter4parts import GPTModel, generate_text_simple
+
+import parts
 
 GPT_CONFIG_124M = {
     'vocab_size': 50257,
-    'context_length': 1024,
+    'context_length': 256,
     'emb_dim': 768,
     'n_heads': 12,
     'n_layers': 12,
@@ -21,6 +24,18 @@ def _apply_text_verification():
     torch.manual_seed(123)
     model = GPTModel(GPT_CONFIG_124M)
     model.eval()
+
+    start_context = 'Every effort moves you'
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    token_ids = generate_text_simple(
+        model,
+        parts.text_to_token_ids(start_context, tokenizer),
+        max_new_tokens=10,
+        context_size=GPT_CONFIG_124M['context_length']
+    )
+
+    print('Output text:\n', parts.token_ids_to_text(token_ids, tokenizer))
 
 
 if __name__ == '__main__':
--- chapter5/parts/_funcs.py
+++ chapter5/parts/_funcs.py
@@ -4,7 +4,7 @@ import torch
 
 
 def text_to_token_ids(text, tokenizer):
-    encoded = tokenizer.encode(text, allow_special={'<|endoftext|>'})
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
     encoded_tensor = torch.tensor(encoded).unsqueeze(0)
 
     return encoded_tensor
