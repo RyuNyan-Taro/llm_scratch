3cebd3ab7082c7d878272b7af551d3733aadcbe2
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-31 15:00:16 +0900

    add common encode and decode process

---
+++ chapter5/parts/_funcs.py
@@ -0,0 +1,16 @@
+__all__ = ['text_to_token_ids', 'token_ids_to_text']
+
+import torch
+
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allow_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+
+    return encoded_tensor
+
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+
+    return tokenizer.decode(flat.tolist())
