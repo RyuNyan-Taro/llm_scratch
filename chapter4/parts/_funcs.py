c2f6c6e5d51620734554ec8626bc54edc42d4910
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-31 14:28:31 +0900

    add generate_text_simple function

--- chapter4/parts/_funcs.py
+++ chapter4/parts/_funcs.py
@@ -1,4 +1,4 @@
-__all__ = ['print_gradients']
+__all__ = ['print_gradients', 'generate_text_simple']
 
 import torch
 from torch import nn
@@ -16,4 +16,22 @@ def print_gradients(model, x):
     for name, param in model.named_parameters():
         if 'weight' in name:
             print(f'{name} has gradient mean of {param.grad.abs().mean().item()}')
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    """Generates text by repeatedly sampling from model outputs"""
+
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+
+        prob = nn.functional.softmax(logits, dim=-1)
+
+        idx_next = torch.argmax(prob, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    return idx
 
