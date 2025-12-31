e8ffc0be7660791ff4e089add7abd2362e94ceab
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-31 16:48:46 +0900

    add initial batch result

--- chapter5/apply_chapter5.py
+++ chapter5/apply_chapter5.py
@@ -17,7 +17,9 @@ GPT_CONFIG_124M = {
 
 def main():
 
-    _apply_text_verification()
+    # _apply_text_verification()
+
+    _apply_loss()
 
 
 def _apply_text_verification():
@@ -36,6 +38,28 @@ def _apply_text_verification():
     )
 
     print('Output text:\n', parts.token_ids_to_text(token_ids, tokenizer))
+
+
+def _apply_loss():
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    inputs = torch.tensor([[16833, 3626, 6100], [40, 1107, 588]])
+    targets = torch.tensor([[3626, 6100, 345], [1107, 588, 11311]])
+
+    with torch.no_grad():
+        logits = model(inputs)
+
+    probas = torch.softmax(logits, dim=-1)
+    print(probas.shape)
+
+    token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+    print('token_ids:', token_ids)
+
+    print(f'targets batch1: {parts.token_ids_to_text(targets[0], tokenizer)}')
+    print(f'output batch1: {parts.token_ids_to_text(token_ids[0].flatten(), tokenizer)}')
 
 
 if __name__ == '__main__':
