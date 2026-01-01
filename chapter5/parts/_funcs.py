86cffe5110d0afee14bdb9f2f04c656ccacddd75
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-01 21:37:14 +0900

    add generate function to apply temperature and top_k logic

--- chapter5/parts/_funcs.py
+++ chapter5/parts/_funcs.py
@@ -6,7 +6,8 @@ __all__ = [
     'calc_loss_loader',
     'train_model_simple',
     'plot_losses',
-    'softmax_with_temperature'
+    'softmax_with_temperature',
+    'generate'
 ]
 
 import tiktoken
@@ -177,3 +178,34 @@ def softmax_with_temperature(logits, temperature: float):
     scaled_logits = logits / temperature
 
     return torch.softmax(scaled_logits, dim=0)
+
+
+def generate(model, idx, max_new_tokens, context_size, temperature: float = 0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                condition=logits < min_val,
+                input=torch.tensor(float('-inf')),
+                other=logits
+            )
+
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        if idx_next == eos_id:
+            break
+
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    return idx
