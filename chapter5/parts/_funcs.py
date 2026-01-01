7c778fd3427c0480ede77178d0e53592015d1f6c
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-01 16:50:37 +0900

    modify to apply train_model_simple function

--- chapter5/parts/_funcs.py
+++ chapter5/parts/_funcs.py
@@ -1,4 +1,11 @@
-__all__ = ['text_to_token_ids', 'token_ids_to_text', 'create_dataloader_v1', 'calc_loss_batch', 'calc_loss_loader']
+__all__ = [
+    'text_to_token_ids',
+    'token_ids_to_text',
+    'create_dataloader_v1',
+    'calc_loss_batch',
+    'calc_loss_loader',
+    'train_model_simple'
+]
 
 import tiktoken
 import torch
@@ -87,3 +94,33 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
 
     return total_loss / num_batches
 
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
+
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+
+            loss.backward()
+            optimizer.step()
+
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Epoch {epoch+1}: train loss: {train_loss:.2f}, val loss: {val_loss:.2f}")
+
+        generate_and_print_sample(model, tokenizer, device, start_context)
+
+    return train_losses, val_losses, track_tokens_seen
