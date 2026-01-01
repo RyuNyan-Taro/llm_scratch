d58b8e7f9261b06ec49c95b1425319ed524c7ec2
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-01 21:10:30 +0900

    change to apply decoding ideas

--- chapter5/apply_chapter5.py
+++ chapter5/apply_chapter5.py
@@ -26,7 +26,9 @@ def main():
 
     # _apply_calculate_loss()
 
-    _apply_training_process()
+    # _apply_training_process()
+
+    _apply_decoding_ideas()
 
 
 def _get_loaders():
@@ -190,6 +192,28 @@ def _apply_training_process():
 
     epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
     parts.plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+
+def _apply_decoding_ideas():
+    vocab = {
+        "closer": 0,
+        "every": 1,
+        "effort": 2,
+        "forward": 3,
+        "inches": 4,
+        "moves": 5,
+        "pizza": 6,
+        "toward": 7,
+        "you": 8
+    }
+
+    inverse_vocab = {v: k for k, v in vocab.items()}
+
+    next_token_logits = torch.tensor([4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79])
+
+    probas = torch.softmax(next_token_logits, dim=0)
+    next_token_id = torch.argmax(probas).item()
+    print(inverse_vocab[next_token_id])
 
 
 if __name__ == '__main__':
