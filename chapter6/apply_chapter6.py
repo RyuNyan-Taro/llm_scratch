2a75bf8528968b852d3f74fef319d3f53bedca30
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-02 09:27:22 +0900

    modify to show downloaded tsv file

--- chapter6/apply_chapter6.py
+++ chapter6/apply_chapter6.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pandas as pd
+
 import parts
 
 
@@ -15,6 +17,11 @@ def _apply_file_download():
     data_file_path = Path(extracted_path) / 'SMSSpamCollection.tsv'
 
     parts.download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+
+    df = pd.read_csv(data_file_path, sep='\t', header=None, names=['Label', 'Text'])
+
+    print(df.info())
+    print(df)
 
 
 if __name__ == "__main__":
