a5b5564a4334d85ac496dacba8647ee005a07e11
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-02 19:40:50 +0900

    add classify_review function

--- chapter6/parts/_funcs.py
+++ chapter6/parts/_funcs.py
@@ -1,4 +1,4 @@
-__all__ = ['calc_accuracy_loader', 'calc_loss_loader', 'train_classifier_simple', 'plot_values']
+__all__ = ['calc_accuracy_loader', 'calc_loss_loader', 'train_classifier_simple', 'plot_values', 'classify_review']
 
 import torch
 from matplotlib import pyplot as plt
@@ -106,3 +106,25 @@ def plot_values(epochs_seen, examples_seen, train_values, val_values, label='los
 
     fig.tight_layout()
     plt.show()
+
+
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
+    model.eval()
+
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[1]
+
+    inputs_ids = input_ids[:min(max_length, supported_context_length)]
+
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+
+    input_tensor = torch.tensor(
+        input_ids, device=device
+    ).unsqueeze(0)
+
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]
+
+    predicted_label = torch.argmax(logits, dim=-1).item()
+
+    return "spam" if predicted_label == 1 else "not spam"
