1b9910f2b2a4ecefe22bc66940c7214a8d321784
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-02 13:53:39 +0900

    add create_balanced_dataset function

--- chapter6/parts/_file.py
+++ chapter6/parts/_file.py
@@ -1,9 +1,11 @@
-__all__ = ['download_and_unzip_spam_data']
+__all__ = ['download_and_unzip_spam_data', 'create_balanced_dataset']
 
 import urllib.request
 import zipfile
 import os
 from pathlib import Path
+
+import pandas as pd
 
 
 def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
@@ -21,3 +23,12 @@ def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     original_file_path = Path(extracted_path) / 'SMSSpamCollection'
     os.rename(original_file_path, data_file_path)
     print(f'File downloaded and extracted to {data_file_path}.')
+
+
+def create_balanced_dataset(df: pd.DataFrame):
+    num_spam = df[df['Label'] == 'spam'].shape[0]
+    ham_subset = df[df['Label'] == 'ham'].sample(num_spam, random_state=123)
+
+    balanced_df = pd.concat([ham_subset, df[df['Label'] == 'spam']])
+
+    return balanced_df
