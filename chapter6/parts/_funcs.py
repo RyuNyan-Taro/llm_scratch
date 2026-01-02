45c4e481cb8b10af7d3bff40e5750fccfc7c48b7
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-02 16:34:19 +0900

    add classification process function

--- chapter6/parts/_funcs.py
+++ chapter6/parts/_funcs.py
@@ -1,8 +1,8 @@
-__all__ = ['calc_accuracy_loader', 'calc_loss_loader']
+__all__ = ['calc_accuracy_loader', 'calc_loss_loader', 'train_classifier_simple']
 
 import torch
 
-from .chapter5parts import calc_loss_batch
+from .chapter5parts import calc_loss_batch, evaluate_model
 
 
 def calc_accuracy_loader(data_loader, model, device, num_batches=None):
@@ -51,3 +51,38 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
             break
 
     return total_loss / num_batches
+
+
+def train_classifier_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter):
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f'Epoch {epoch+1}: (Step {global_step:06d}) train loss: {train_loss:.3}, val loss: {val_loss:.3f}')
+
+            train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
+            val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
+
+            print(f'train accuracy: {train_accuracy*100:.2f}% | ', end="")
+            print(f'val accuracy: {val_accuracy*100:.2f}%')
+            train_accs.append(train_accuracy)
+            val_accs.append(val_accuracy)
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
