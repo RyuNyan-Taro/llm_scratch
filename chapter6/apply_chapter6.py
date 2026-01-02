11bf3c03094cd7b18c6ac1ba4f417caeaef7ff77
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-02 09:25:07 +0900

    add file download process

--- chapter6/apply_chapter6.py
+++ chapter6/apply_chapter6.py
@@ -1,5 +1,20 @@
+from pathlib import Path
+
+import parts
+
+
 def main():
-    print('start chapter6')
+
+    _apply_file_download()
+
+
+def _apply_file_download():
+    url = 'https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip'
+    zip_path = 'sms_spam_collection.zip'
+    extracted_path = 'sms_spam_collection'
+    data_file_path = Path(extracted_path) / 'SMSSpamCollection.tsv'
+
+    parts.download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
 
 
 if __name__ == "__main__":
