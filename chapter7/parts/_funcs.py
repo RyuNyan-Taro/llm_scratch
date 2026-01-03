4f197b6145d4efe384a9816c0854b09366e2503f
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-03 16:08:32 +0900

    add custom_collate_draft_2 to return the target tensor with inputs

--- chapter7/parts/_funcs.py
+++ chapter7/parts/_funcs.py
@@ -1,7 +1,8 @@
 __all__ = [
     'download_and_load_file',
     'format_input',
-    'custom_collate_draft_1'
+    'custom_collate_draft_1',
+    'custom_collate_draft_2'
 ]
 
 import json
@@ -75,3 +76,25 @@ def custom_collate_draft_1(batch, pad_token_id=50256, device='cpu'):
 
     return inputs_tensor
 
+
+def custom_collate_draft_2(batch, pad_token_id=50256, device='cpu'):
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+
+    return inputs_tensor, targets_tensor
