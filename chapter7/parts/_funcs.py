bbf4ec267b1e8370a65aecfe7941f91ea953b35f
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-03 20:43:52 +0900

    add custom_collate_fn

--- chapter7/parts/_funcs.py
+++ chapter7/parts/_funcs.py
@@ -2,7 +2,8 @@ __all__ = [
     'download_and_load_file',
     'format_input',
     'custom_collate_draft_1',
-    'custom_collate_draft_2'
+    'custom_collate_draft_2',
+    'custom_collate_fn'
 ]
 
 import json
@@ -90,6 +91,41 @@ def custom_collate_draft_2(batch, pad_token_id=50256, device='cpu'):
 
         inputs = torch.tensor(padded[:-1])
         targets = torch.tensor(padded[1:])
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+
+    return inputs_tensor, targets_tensor
+
+
+def custom_collate_fn(batch, pad_token_id=50256, ignore_index=-100,
+                      allowed_max_length=None, device='cpu'):
+
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
+
+    # Iterates batch; pads; masks targets; truncates if needed
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
 
         inputs_lst.append(inputs)
         targets_lst.append(targets)
