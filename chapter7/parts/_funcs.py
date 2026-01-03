6ffacf2e4c2bc2c73c0a336678373e4701e7bc4e
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-03 09:12:07 +0900

    create parts module and add donwload_and_load_file function

---
+++ chapter7/parts/_funcs.py
@@ -0,0 +1,22 @@
+__all__ = ['download_and_load_file']
+
+import json
+import os.path
+import urllib
+
+
+def download_and_load_file(file_path: str, url: str):
+    if not os.path.exists(file_path):
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode('utf-8')
+        with open(file_path, 'w', encodeing='utf-8') as file:
+            file.write(text_data)
+
+    else:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text_data = file.read()
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    return data
